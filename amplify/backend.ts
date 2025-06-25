ae3ad5646f1218b116214805e0bf542475a76dba
Joetx14 <jgreen@farewellfinder.com>
2025-06-25 10:45:32 -0500

    yaml

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,11 +1,21 @@
+/// amplify/backend.ts
 import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource';
+// Ensure the file './auth/resource.ts' exists and exports 'auth'
 import { data } from './data/resource';
+import { auth } from './auth/resource';
 
-/**
- * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
- */
-defineBackend({
+import { autoConfirmUserFunction } from './functions/autoConfirmUser/resource';
+import { sendCollaboratorInvite } from './functions/sendCollaboratorInvite/resource';
+import { createStripePaymentIntentFunction } from './functions/createStripePaymentIntent/resource';
+import { stripeWebhookHandlerFunction } from './functions/stripeWebhookHandler/resource';
+
+
+
+export default defineBackend({
   auth,
   data,
+  autoConfirmUserFunction,
+  sendCollaboratorInvite,
+  createStripePaymentIntentFunction,
+  stripeWebhookHandlerFunction,
 });
