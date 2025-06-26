097e990ab9991cb31c6870845eccb7eea9f0c4d5
Joetx14 <jgreen@farewellfinder.com>
2025-06-26 13:06:31 -0500

    passworfd

--- src/components/LandingPages/CreatePassword.jsx
+++ src/components/LandingPages/CreatePassword.jsx
@@ -5,7 +5,7 @@ import StandardLayout from '../StandardLayout';
 import FormBox from '../FormBox';
 import TextInput from '../TextInput';
 import PrimaryButton from '../buttons/PrimaryButton';
-import { getCurrentUser } from 'aws-amplify/auth';
+
 import '../../styles/OnboardingForms.css';
 
 const CreatePassword = () => {
@@ -78,7 +78,6 @@ const CreatePassword = () => {
             const { isSignedIn } = await signIn({ username: email.trim(), password: password });
 
             if (isSignedIn) {
-              await getCurrentUser();
               localStorage.removeItem('isNewUser');
               navigate('/welcome', { state: { isNewUser: false } });
               return;
